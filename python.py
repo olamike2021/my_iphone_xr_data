882cd42e7cc0caa19c07c967ac9d62fa75ff7850
olamike2021 <agboolafolorunso4@gmail.com>
2023-04-08 16:10:33 +0200

    my iphone xr data to used for my python code in master serve

---
+++ python.py
@@ -0,0 +1,29 @@
+for i in range(1,8,3):
+
+    print(i,end='')
+
+ n=4 
+def f1(num):
+    return num ** 4
+n = f1(n)
+print(n)   
+
+x = 0
+while x < 20:
+    x = x + 3
+print(x)
+
+brand = " "
+age = 1
+number = []
+python = {}
+text = ()
+
+study_2 = 20
+print(study_2)
+
+name = "iphone"
+brand = "apple"
+x = 14
+colour "black"
+
